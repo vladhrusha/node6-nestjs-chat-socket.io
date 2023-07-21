6285e1f58276b221690e776421d76e49fd4da7fc
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-07-21 21:47:20 +0300

    chnaged chatHistory to nested dictionary

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,68 +1,43 @@
 import {
-  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Server, Socket } from 'socket.io';
-
-interface UserMessages {
-  socketId: string;
-  postedMessagesId: string[];
-}
 
 @WebSocketGateway()
 export class ChatGateway {
   @WebSocketServer()
   server: Server;
-  users: { [key: string]: string } = {};
-  userMessages: UserMessages[] = [];
-
+  connectedUsers: { [socketId: string]: string } = {};
+  userChatHistory: { [socketId: string]: { [messageId: string]: string } } = {};
   //conn
   handleConnection(socket: Socket) {
-    this.users[socket.id] =
+    this.connectedUsers[socket.id] =
       socket.handshake.query.username?.toString() ||
       `User${Math.floor(Math.random() * 100000)}`;
-
-    const newUser: UserMessages = {
-      socketId: socket.id,
-      postedMessagesId: [],
-    };
-    this.userMessages.push(newUser);
-
-    socket.emit('users', Object.values(this.users));
+    this.userChatHistory[socket.id] = {};
+    socket.emit('users', Object.values(this.connectedUsers));
     this.server.emit(
       'message',
-      `${this.users[socket.id]} has joined the chat.`,
+      `${this.connectedUsers[socket.id]} has joined the chat`,
     );
   }
   //disconn
   handleDisconnect(socket: Socket) {
-    const disconnectedUser = this.users[socket.id];
-    delete this.users[socket.id];
-    const userIndex = this.userMessages.findIndex(
-      (user) => user.socketId === socket.id,
-    );
-    if (userIndex !== -1) {
-      this.userMessages.splice(userIndex, 1);
-    }
+    const disconnectedUser = this.connectedUsers[socket.id];
+    delete this.connectedUsers[socket.id];
+    delete this.userChatHistory[socket.id];
+
     this.server.emit('message', `${disconnectedUser} has left the chat`);
   }
 
   //message
   @SubscribeMessage('message')
   handleMessage(socket: Socket, message: { id: string; content: string }) {
-    const sender = this.users[socket.id];
+    const sender = this.connectedUsers[socket.id];
     const { id, content } = message;
-
-    this.userMessages.forEach((userMessage) => {
-      if (userMessage.socketId === socket.id) {
-        userMessage.postedMessagesId.push(id);
-      }
-    });
+    this.userChatHistory[socket.id][id] = content;
     this.server.emit('message', { sender, content });
   }
 
@@ -74,31 +49,25 @@ export class ChatGateway {
   ) {
     const { id, content } = message;
 
-    const userIndex = this.userMessages.findIndex(
-      (user) => user.socketId === socket.id,
-    );
-    const messageIndex = this.userMessages[
-      userIndex
-    ].postedMessagesId.findIndex((messageId) => messageId === id);
-    if (messageIndex !== -1) {
+    if (this.userChatHistory[socket.id][id]) {
+      this.userChatHistory[socket.id][id] = content;
       this.server.emit('messageUpdated', { id, content });
-    } else socket.emit('messageUpdated', 'error - message not found');
+    } else {
+      socket.emit('messageUpdated', 'error - message not found');
+    }
   }
 
   //delete message
   @SubscribeMessage('deleteMessage')
   handleDeleteMessage(socket: Socket, message: { id: string }) {
-    const socketId = socket.id;
-    const { id } = message;
-    const userIndex = this.userMessages.findIndex(
-      (user) => user.socketId === socket.id,
-    );
-    const messageIndex = this.userMessages[
-      userIndex
-    ].postedMessagesId.findIndex((messageId) => messageId === id);
-    if (messageIndex !== -1) {
-      this.userMessages[userIndex].postedMessagesId.splice(messageIndex, 1);
-      this.server.emit('messageDeleted', { id, socketId });
-    } else socket.emit('messageDeleted', 'error - message not found');
+    if (this.userChatHistory[socket.id][message.id]) {
+      delete this.userChatHistory[socket.id][message.id];
+      this.server.emit('messageDeleted', {
+        messageId: message.id,
+        socketId: socket.id,
+      });
+    } else {
+      socket.emit('messageDeleted', 'error - message not found');
+    }
   }
 }
