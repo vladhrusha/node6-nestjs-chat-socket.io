6467102920caae3421552eada6e90a2e0096dbb3
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-07-21 19:13:47 +0300

    task 6.5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventsModule } from './modules/events/events.module';
+import { ChatModule } from './modules/chat/chat.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/chat/chat.gateway.ts
@@ -0,0 +1,97 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server, Socket } from 'socket.io';
+
+interface UserMessages {
+  socketId: string;
+  postedMessagesId: string[];
+}
+
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+  users: { [key: string]: string } = {};
+  userMessages: UserMessages[] = [];
+
+  //conn
+  handleConnection(socket: Socket) {
+    this.users[socket.id] =
+      socket.handshake.query.username?.toString() ||
+      `User${Math.floor(Math.random() * 100000)}`;
+
+    const newUser: UserMessages = {
+      socketId: socket.id,
+      postedMessagesId: [],
+    };
+    this.userMessages.push(newUser);
+
+    socket.emit('users', Object.values(this.users));
+    this.server.emit(
+      'message',
+      `${this.users[socket.id]} has joined the chat.`,
+    );
+  }
+  //disconn
+  handleDisconnect(socket: Socket) {
+    const disconnectedUser = this.users[socket.id];
+    delete this.users[socket.id];
+    const userIndex = this.userMessages.findIndex(
+      (user) => user.socketId === socket.id,
+    );
+    if (userIndex !== -1) {
+      this.userMessages.splice(userIndex, 1);
+    }
+    this.server.emit('message', `${disconnectedUser} has left the chat`);
+  }
+
+  //message
+  @SubscribeMessage('message')
+  handleMessage(socket: Socket, message: { id: string; content: string }) {
+    const sender = this.users[socket.id];
+    const { id, content } = message;
+
+    this.userMessages.forEach((userMessage) => {
+      if (userMessage.socketId === socket.id) {
+        userMessage.postedMessagesId.push(id);
+      }
+    });
+    this.server.emit('message', { sender, content });
+  }
+
+  //update message
+  @SubscribeMessage('updateMessage')
+  handleUpdateMessage(
+    socket: Socket,
+    message: { content: string; id: string },
+  ) {
+    const socketId = socket.id;
+    const { id, content } = message;
+
+    this.server.emit('messageUpdated', { id, content, socketId });
+  }
+
+  //delete message
+  @SubscribeMessage('deleteMessage')
+  handleDeleteMessage(socket: Socket, message: { id: string }) {
+    const socketId = socket.id;
+    const { id } = message;
+    const userIndex = this.userMessages.findIndex(
+      (user) => user.socketId === socket.id,
+    );
+    const messageIndex = this.userMessages[
+      userIndex
+    ].postedMessagesId.findIndex((messageId) => messageId === id);
+    if (messageIndex !== -1) {
+      this.userMessages[userIndex].postedMessagesId.splice(messageIndex, 1);
+    }
+    this.server.emit('messageDeleted', { id, socketId });
+  }
+}
